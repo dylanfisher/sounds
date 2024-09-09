b311ca9c8ade074bb3f90777368d7cd6aea2adbf
dylanfisher <hi@dylanfisher.com>
2024-09-09 10:32:07 -0700

    Update mem_cache_store for dokku env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,11 +55,8 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  if ENV["MEMCACHEDCLOUD_SERVERS"]
-    config.cache_store = :mem_cache_store,
-                         ENV["MEMCACHEDCLOUD_SERVERS"].split(","), {
-                           username: ENV["MEMCACHEDCLOUD_USERNAME"],
-                           password: ENV["MEMCACHEDCLOUD_PASSWORD"] }
+  if ENV["MEMCACHED_URL"]
+    config.cache_store = :mem_cache_store, ENV["MEMCACHED_URL"]
   end
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
